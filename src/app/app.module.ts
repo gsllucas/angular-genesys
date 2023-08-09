6804977ac9941929e43cfcb0a39b9de6604c6fd2
gsllucas <lucas.goncalves@coopersystem.com.br>
2023-08-08 21:07:50 -0300

    :construction: Set up initial files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DividerComponent } from './shared/components/divider/divider.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { HeaderModule } from './layouts/components/header/header.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DividerComponent,
+    InputTextModule,
+    AutoCompleteModule,
+    ReactiveFormsModule,
+    FormsModule,
+    CardModule,
+    HeaderModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
