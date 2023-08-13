48b939548ae00286996bcd3dd8909cad741eded5
gsllucas <lucas.goncalves@coopersystem.com.br>
2023-08-13 08:52:09 -0300

    :construction: Working on dashboard component

---
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
+})
+export class DashboardComponent {
+  assets = new Array(10);
+}
